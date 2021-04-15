ea71d3536dbb98b08d19c8d0648307cb8de0b0d5
Игорь Голяк <golyak.03@inbox.ru>
2021-04-15 12:47:05 +0700

    15.04.21

---
+++ 15.04.21/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._04._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int r = Convert.ToInt32(Console.ReadLine());
+            int a = r / 10;
+            int b = r % 10;
+            Console.WriteLine(a + b);
+            if ((a + b) % 2 == 0)
+            {
+                Console.WriteLine("Четкое");
+            }
+            else
+            {
+                Console.WriteLine("Не Четкое");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Rdd/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int r = Convert.ToInt32(Console.ReadLine());
+            int a = r / 10;
+            int b = r % 100;
+            int c = r / 10 & 10;
+            Console.WriteLine(a = b);
+
+            if ((a + b) % 3 == 0)
+            {
+                Console.WriteLine("Polindrom");
+            }
+            else
+            {
+                Console.WriteLine("Ne Polindrom");
+            }
+
+            if (a == b)
+            {
+                Console.WriteLine("No Deletsa 3");
+            }
+            else
+            {
+                Console.WriteLine("Deletsa 3");
+            }
+            
+            Console.ReadKey();
+
+
+            
+           
+
+        }
+    }
+}
